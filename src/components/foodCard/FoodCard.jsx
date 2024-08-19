6a75708e36ca73b271cd953af7646991bbbd14f1
Ayberk <ayberkcetinkaya54@gmail.com>
2024-08-19 11:11:47 +0300

    Update FoodCard.jsx

--- src/components/foodCard/FoodCard.jsx
+++ src/components/foodCard/FoodCard.jsx
@@ -1,6 +1,16 @@
+import { useState } from "react";
 import "./foodCard.css";
 
 function FoodCard() {
+  const initialData = {
+    isim: "Position Absolute Acı Pizza",
+    id: 1,
+    fiyat: 85.5,
+    degerlendirme: "4.9       (200)",
+    acıklama:
+      "Frontent Dev olarak hala position:absolute kullanıyorsan bu çok acı pizza tam sana göre. Pizza, domates, peynir ve genellikle çeşitli diğer malzemelerle kaplanmış, daha sonra geleneksel olarak odun ateşinde bir fırında yüksek sıcaklıkta pişirilen, genellikle yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan oluşan İtalyan kökenli lezzetli bir yemektir. . Küçük bir pizzaya bazen pizzetta denir. ",
+  };
+  const [pizzaIcerik, setPizzaIcerik] = useState(initialData);
   return (
     <div className='food-card-container'>
       <div className='food-card-section'>
@@ -13,19 +23,11 @@ function FoodCard() {
           Anasayfa - <span>Sipariş Oluştur</span>{" "}
         </span>
         <div className='food-card'>
-          <h2>Position Absolute Acı Pizza</h2>
+          <h2>{pizzaIcerik.isim}</h2>
           <div className='food-card-fiyat'>
-            <p>85.5TL</p>
-            <div className='food-card-yorum'>4.9 (200)</div>
-            <p className='food-card-acıklama'>
-              Frontent Dev olarak hala position:absolute kullanıyorsan bu çok
-              acı pizza tam sana göre. Pizza, domates, peynir ve genellikle
-              çeşitli diğer malzemelerle kaplanmış, daha sonra geleneksel olarak
-              odun ateşinde bir fırında yüksek sıcaklıkta pişirilen, genellikle
-              yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan oluşan
-              İtalyan kökenli lezzetli bir yemektir. . Küçük bir pizzaya bazen
-              pizzetta denir.
-            </p>
+            <p>{pizzaIcerik.fiyat} ₺</p>
+            <div className='food-card-yorum'>{pizzaIcerik.degerlendirme}</div>
+            <p className='food-card-acıklama'>{pizzaIcerik.acıklama}</p>
           </div>
         </div>
       </div>
